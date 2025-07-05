19a2fd3ce6067a79240d04ef70d6e680f447fce2
Gary Clark <gary.clark@salesforce.com>
2025-07-05 14:38:42 -0700

    Initial Checkin

---
+++ src/main/java/kbdcat/rogueai/RogueaiApplication.java
@@ -0,0 +1,13 @@
+package kbdcat.rogueai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogueaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RogueaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kbdcat/rogueai/RogueaiApplicationTests.java
@@ -0,0 +1,13 @@
+package kbdcat.rogueai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogueaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
